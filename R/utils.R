280a53d67eeee07ae1ee60bcf97352e8303e390b
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-09-26 21:35:52 -0400

    add more utils

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,33 @@
 exattr <- function(x, which) {
   attr(x, which = which, exact = TRUE)
 }
+
+is_integerish <- function(x, bool = getOption("fact.bool.integer", FALSE)) {
+  if (is.integer(x) | (isTRUE(bool) & is.logical(x))) {
+    return(TRUE)
+  }
+
+  if (!is.double(x)) {
+    return(FALSE)
+  }
+
+  x <- x[!is.na(x)]
+
+  if (!length(x)) {
+    return(TRUE)
+  }
+
+  all(x %% 1 == 0)
+}
+
+cat0 <- function(...) {
+  cat(..., sep = "" )
+}
+
+values <- function(x) {
+  exattr(x, "values")
+}
+
+last <- function(x) {
+  x[length(x)]
+}
