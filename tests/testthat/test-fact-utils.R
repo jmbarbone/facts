f4fa2d12dda5b345d14b7e3bf054c15df68152a3
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-11-20 23:05:32 -0500

    [#10] add missing fact-util tests

--- tests/testthat/test-fact-utils.R
+++ tests/testthat/test-fact-utils.R
@@ -24,6 +24,14 @@ test_that("fact_coerce_levels()", {
 
   x <- as.POSIXlt("2021-09-03", tz = "UTC") + 1:3
   expect_equal(fact_coerce_levels(as.character(x)), x)
+
+  exp <- c(1.1, 2.0, NA)
+  obj <- fact_coerce_levels(as.character(exp))
+  expect_equal(obj, exp)
+})
+
+test_that("fact_na_label()", {
+  expect_error(fact_na_label(1:2), class = "factNaLabelError")
 })
 
 test_that("try_numeric()", {
@@ -38,4 +46,12 @@ test_that("try_numeric()", {
 
   x <- c(1, NA, 2)
   expect_identical(try_numeric(as.character(x)), x)
+})
+
+test_that("check_fact()", {
+  expect_true(is.fact(new_fact()))
+  expect_true(is_fact(new_fact()))
+
+  expect_no_error(check_fact(new_fact()))
+  expect_error(check_fact(struct(NULL, "foo")), class = "factNaClassError")
 })
