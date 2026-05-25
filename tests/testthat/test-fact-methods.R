750cf950b80cebc727eef2b2440b90f32c608de2
Jordan Mark Barbone <jmbarbone@gmail.com>
2026-05-25 14:47:21 -0400

    update tests

--- tests/testthat/test-fact-methods.R
+++ tests/testthat/test-fact-methods.R
@@ -11,29 +11,19 @@ test_that("is.na.fact(), works", {
   res <- is.na(x)
   exp <- c(FALSE, FALSE, TRUE, FALSE)
   expect_identical(res, exp)
-
-  x <- fact_na(c(1, 2, NA, 3))
-  res <- is.na(x)
-  exp <- c(FALSE, FALSE, TRUE, FALSE)
-  expect_identical(res, exp)
-
-  x <- fact_na(c("a", "b", "c"))
-  res <- is.na(x)
-  exp <- c(FALSE, FALSE, FALSE)
-  expect_identical(res, exp)
 })
 
 test_that("as.integer.fact() works", {
   x <- fact(c(1, 2, NA, 3))
   res <- as.integer(x)
-  exp <- c(1L, 2L, NA_integer_, 3L)
+  exp <- c(2L, 3L, 1L, 4L)
   expect_identical(res, exp)
 })
 
 test_that("as.integer.fact() works", {
   x <- fact(c(1, 2, NA, 3))
   res <- as.double(x)
-  exp <- c(1, 2, NA_real_, 3)
+  exp <- c(2, 3, 1, 4)
   expect_identical(res, exp)
   expect_identical(as.numeric(x), exp)
 })
@@ -41,11 +31,6 @@ test_that("as.integer.fact() works", {
 test_that("unique.fact() works", {
   x <- fact(c(1, 2, NA, 3, 2))
   exp <- fact(c(1, 2, NA, 3))
-  res <- unique(x)
-  expect_identical(exp, res)
-
-  x <- as_ordered(x)
-  exp <- as_ordered(exp)
   res <- unique(x)
   expect_identical(exp, res)
 })
@@ -68,16 +53,6 @@ test_that("as.character.fact() works", {
   expect_identical(exp, res)
 })
 
-test_that("format.fact()", {
-  obj <- format(fact(1))
-  exp <- "1 [1]"
-  expect_identical(obj, exp)
-
-  obj <- format(fact(c("a", "b", "c")))
-  exp <- c("1 [a]", "2 [b]", "3 [c]")
-  expect_identical(obj, exp)
-})
-
 test_that("pillar_shaft.fact()", {
   expect_no_error(pillar_shaft(fact(1:100)))
 })
@@ -93,9 +68,8 @@ test_that("print.fact()", {
     }
   }
 
-  foo(bar(as_ordered(integer())))
   foo(bar(fact(integer())))
-  foo(bar(fact(1L, range = 0:2)))
+  foo(bar(fact(1L)))
   foo(bar(fact(1:5), max_levels = 5))
   foo(bar(fact(1:100), max_levels = 1))
   foo(bar(fact(1:100), max_levels = 2))
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,24 +1,4 @@
-test_that("is_integerish()", {
-  expect_true(is_integerish(1L))
-  expect_true(is_integerish(1))
-  expect_true(is_integerish(NA_real_))
-
-  expect_false(is_integerish("a"))
-  expect_false(is_integerish(1.1))
-
-  withr::local_options(list(facts.bool.integer = FALSE))
-  expect_false(is_integerish(NA))
-  expect_false(is_integerish(TRUE))
-
-  withr::local_options(list(facts.bool.integer = TRUE))
-  expect_true(is_integerish(NA))
-  expect_true(is_integerish(TRUE))
-})
-
 test_that("values()", {
-  expect_identical(values(factor("a")), "a")
   expect_identical(values(fact("a")), "a")
-
-  expect_null(values("a", strict = TRUE))
-  expect_identical(values("a", strict = FALSE), "a")
+  expect_identical(values(fact(as.Date("1992-12-17"))), as.Date("1992-12-17"))
 })
